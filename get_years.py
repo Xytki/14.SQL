dcbdbbf4384c27a891bc36dbf6bd712db3b8ae76
Xytki <46975352+Xytki@users.noreply.github.com>
2022-10-01 18:54:14 +0200

    Update get_years.py
    I transferred the request processing in the function

--- get_years.py
+++ get_years.py
@@ -1,24 +1,22 @@
 import sqlite3
 
-con = sqlite3.connect('netflix.db')
-cur = con.cursor()
-sqlite_query = ("SELECT release_year, title "
-                "FROM netflix "
-                "WHERE `type`='Movie' "
-                "ORDER BY release_year DESC "
-                "LIMIT 100 ")
 
-cur.execute(sqlite_query)
-results = cur.fetchall()
-con.close()
+def get_data(sql):
+    with sqlite3.connect("netflix.db") as connection:
+        connection.row_factory = sqlite3.Row
+
+        result = connection.execute(sql).fetchall()
+    return result
 
 
-def get_release_year(ot, do):
-    a = []
-    for result in results:
-        if ot <= result[0] <= do:
-            a.append({"title": result[1], "release_year": result[0]})
-    return a
-
+def get_release_year(year1, year2):
+    result = []
+    for item in get_data(sql=f'''SELECT * 
+                    FROM netflix 
+                    WHERE `type`='Movie' 
+                    and release_year between {year1} and {year2} 
+                    LIMIT 100 '''):
+        result.append(dict(item))
+    return result
 
 
